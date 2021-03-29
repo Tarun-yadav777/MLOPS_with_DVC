d6468eada4e70967f3466cf92838edf2fdd9bbad
Tarun <tarun.rao777@gmail.com>
2021-03-29 12:52:26 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Its a MLops Package",
+    author="Tarun",
+    packages=find_packages(),
+    license="MIT License"
+)
+
