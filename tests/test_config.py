a7edc593e6f0addcfa0e078e26e9ad63fc4b4931
Tarun <tarun.rao777@gmail.com>
2021-03-29 13:46:02 +0530

    web app structure

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,13 @@
+import pytest
+
+
+class NotInRange(Exception):
+    def __init__(self, message="Value not in range"):
+        self.message = message
+        super().__init__(self.message)
+
+
 def test_generic():
     a = 2
     b = 2
-    assert a==b
+    assert a == b
